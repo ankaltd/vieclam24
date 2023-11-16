3a6cb817c193ba0ff6ad50af1681f87fab856356
duongcgc <duongcgc@gmail.com>
2023-11-16 21:59:32 +0700

    u

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: [
-    './src/**/*.php',
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  content: ["./src/**/*.php", "./src/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {
       colors: {
-        primary: '#3490dc',
-        secondary: '#ffed4a',
+        primary: "#3490dc",
+        secondary: "#ffed4a",
       },
       fontSize: {
-        '7xl': '5rem',
+        "7xl": "5rem",
       },
       // ...Thêm các mở rộng khác nếu cần
     },
   },
   variants: {
     extend: {
-      backgroundColor: ['active'],
-      textColor: ['visited'],
+      backgroundColor: ["active"],
+      textColor: ["visited"],
       // ...Thêm các variants khác nếu cần
     },
   },
   plugins: [],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-}
-
+  module: {},
+};
